d37d0075f607fb629cc6f5f4f0fb489efdd33cb4
Gabby Recny <gjtrecny@gmail.com>
2023-09-11 17:37:37 -0600

    Create Search component

---
+++ src/Search/Search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Search.css';
+
+const Search = () => {
+    return (
+        <section className="search-contianer">
+            <p>Search</p>
+        </section>
+    )
+}
+
+export { Search };
