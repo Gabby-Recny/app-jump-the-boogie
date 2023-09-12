5f606ecd99a32ec2e73c1410f2d73e0038d928bc
Gabby Recny <gjtrecny@gmail.com>
2023-09-11 18:53:15 -0600

    Create form input for search

--- src/Search/Search.tsx
+++ src/Search/Search.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import './Search.css';
+import searchIcon from '../images/searchIcon.svg';
 
 const Search = () => {
+    const [ searchInput, setSearchInput ] = useState('');
+    const handleSubmission = (event: any) => {
+        event.preventDeault()
+        console.log('You have hit submit', searchInput)
+    }
+
     return (
         <section className="search-container">
-            <p>Search</p>
+            <form>
+                <label>
+                    Enter your location:
+                </label>
+                <div>
+                    <input type="text" 
+                        name="location" 
+                        value={searchInput}
+                        onChange={event => setSearchInput(event.target.value)}
+                    />
+                    <button 
+                        className='search-btn'
+                        type="submit"
+                        onClick={(e) => handleSubmission(e)}>
+                        <img className="search-icon" src={searchIcon} alt="search icon"></img>
+                    </button>
+                </div>
+            </form>
         </section>
     )
 }
